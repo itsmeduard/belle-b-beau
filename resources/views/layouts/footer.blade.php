137edf2d3d046b7faac014f509e4f64191f12572
Filsen Eduard Valdez <fico.valdez@up.phinma.edu.ph>
2021-03-22 01:38:56 +0800

    March 22 Commit 1:36an

--- resources/views/layouts/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="bg-white sticky-footer">
-    <div class="container my-auto">
-        <div class="text-center my-auto copyright">Designed by Neime Warriors © </div>
-        <div class="text-center my-auto copyright">2021 Belle n Beau.&nbsp;All Rights Reserved.<br></div>
-    </div>
-</footer>
